d78d227056a021e51fe7581649704a9c95fea7b9
hosea-lee <hosea@zigbang.com>
2020-04-13 23:59:23 +0900

    미사용 파일 삭제

--- packages/app/src/core/navigation/options.tsx
+++ packages/app/src/core/navigation/options.tsx
@@ -1,93 +1,89 @@
+import {MenuContainer} from '@src/containers/menu'
 import React from 'react'
-import { Alert } from 'react-native'
+import {Alert, View} from 'react-native'
 import {
-  NavigationParams,
-  NavigationScreenProps,
+	NavigationParams,
+	NavigationScreenProps
 } from 'react-navigation'
-import { EcommerceHeader } from '@src/components/ecommerce'
-import { MenuContainer } from '@src/containers/menu'
-import { ArrowIosBackFill } from '@src/assets/icons'
-import { TopNavigationBar } from './components/topNavigationBar.component'
+import {KEY_NAVIGATION_BACK} from './constants'
 import {
-  getCurrentRouteState,
-  isRootRoute,
-  NavigationRouteState,
-  getCurrentRouteIndex,
+	getCurrentRouteState,
+	NavigationRouteState
 } from './util'
-import { KEY_NAVIGATION_BACK } from './constants'
 
 export type TopNavigationElement = React.ReactElement<any>
 export type BottomNavigationElement = React.ReactElement<any>
 
 export interface TopNavigationParams extends NavigationParams {
-  header: (props: NavigationScreenProps) => TopNavigationElement | null
+	header: (props: NavigationScreenProps) => TopNavigationElement | null
 }
 
 export interface BottomNavigationParams extends NavigationParams {
-  bottomNavigation: (props: NavigationScreenProps) => BottomNavigationElement | null
+	bottomNavigation: (props: NavigationScreenProps) => BottomNavigationElement | null
 }
 
 const MenuTopNavigationParams: TopNavigationParams = {
-  header: null
-  // header: (props: NavigationScreenProps): TopNavigationElement => {
-  //   // @ts-ignore (private API)
-  //   const { routeName } = getCurrentRouteState(props.navigation)
-  //   const index: number = getCurrentRouteIndex(props.navigation)
-  //
-  //   return (
-  //     <TopNavigationBar
-  //       {...props}
-  //       title={routeName}
-  //       backIcon={isRootRoute(index) && ArrowIosBackFill}
-  //       onBackPress={() => {
-  //         props.navigation.goBack(KEY_NAVIGATION_BACK)
-  //       }}
-  //     />
-  //   )
-  // },
+	header: () => null
+	// header: (props: NavigationScreenProps): TopNavigationElement => {
+	//   // @ts-ignore (private API)
+	//   const { routeName } = getCurrentRouteState(props.navigation)
+	//   const index: number = getCurrentRouteIndex(props.navigation)
+	//
+	//   return (
+	//     <TopNavigationBar
+	//       {...props}
+	//       title={routeName}
+	//       backIcon={isRootRoute(index) && ArrowIosBackFill}
+	//       onBackPress={() => {
+	//         props.navigation.goBack(KEY_NAVIGATION_BACK)
+	//       }}
+	//     />
+	//   )
+	// },
 }
 
 const EcommerceMenuTopNavigationParams: TopNavigationParams = {
-  header: (props: NavigationScreenProps): TopNavigationElement => {
-    const state: NavigationRouteState = getCurrentRouteState(props.navigation)
+	header: (props: NavigationScreenProps): TopNavigationElement => {
+		const state: NavigationRouteState = getCurrentRouteState(props.navigation)
 
-    const onBackPress = () => {
-      props.navigation.goBack(KEY_NAVIGATION_BACK)
-    }
+		const onBackPress = () => {
+			props.navigation.goBack(KEY_NAVIGATION_BACK)
+		}
 
-    const onSearchPress = () => {
-      Alert.alert('Search...')
-    }
+		const onSearchPress = () => {
+			Alert.alert('Search...')
+		}
 
-    const onShoppingCartPress = () => {
-      props.navigation.navigate({
-        key: state.routeName,
-        routeName: 'Shopping Cart',
-      })
-    }
+		const onShoppingCartPress = () => {
+			props.navigation.navigate({
+				key: state.routeName,
+				routeName: 'Shopping Cart'
+			})
+		}
 
-    return (
-      <EcommerceHeader
-        title={state.routeName}
-        onBack={onBackPress}
-        onSearch={onSearchPress}
-        onShoppingCart={onShoppingCartPress}
-      />
-    )
-  },
+		return (
+			<View/>
+			// <EcommerceHeader
+			// 	title={state.routeName}
+			// 	onBack={onBackPress}
+			// 	onSearch={onSearchPress}
+			// 	onShoppingCart={onShoppingCartPress}
+			// />
+		)
+	}
 }
 
 const MenuBottomNavigationParams: BottomNavigationParams = {
-  bottomNavigation: (props: NavigationScreenProps): BottomNavigationElement => {
-    return (
-      <MenuContainer {...props} />
-    )
-  },
+	bottomNavigation: (props: NavigationScreenProps): BottomNavigationElement => {
+		return (
+			<MenuContainer {...props} />
+		)
+	}
 }
 
 export const MenuNavigationOptions: NavigationParams = {
-  ...MenuTopNavigationParams,
-  ...MenuBottomNavigationParams,
+	...MenuTopNavigationParams,
+	...MenuBottomNavigationParams
 }
 
 export const SocialNavigationOptions: NavigationParams = MenuTopNavigationParams

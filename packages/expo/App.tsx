8188bbc369b9e8e88bd4f092540f00ef2f2620ea
gskim <kiseon1987@gmail.com>
2020-05-05 14:57:29 +0900

    expo init

---
+++ packages/expo/App.tsx
@@ -0,0 +1,15 @@
+import * as eva from '@eva-design/eva'
+import { ApplicationProvider, Layout, Text } from '@ui-kitten/components'
+import React from 'react'
+
+const HomeScreen = () => (
+  <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+	<Text category='h1'>HOME</Text>
+  </Layout>
+)
+
+export default () => (
+  <ApplicationProvider {...eva} theme={eva.light}>
+	<HomeScreen />
+  </ApplicationProvider>
+)

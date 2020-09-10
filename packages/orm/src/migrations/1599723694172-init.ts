d4c24390d6ade6b2f5f674ea4046b69b93e34a64
gskim <kiseon1987@gmail.com>
2020-09-10 16:49:55 +0900

    orm init

--- packages/orm/src/migrations/1599723694172-init.ts
+++ packages/orm/src/migrations/1599723694172-init.ts
@@ -1,24 +1,24 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class init1588145148340 implements MigrationInterface {
-    name = 'init1588145148340'
+export class init1599723694172 implements MigrationInterface {
+    name = 'init1599723694172'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query("CREATE TABLE `device` (`id` int NOT NULL AUTO_INCREMENT, `is_device` tinyint NOT NULL, `brand` varchar(20) NULL, `manufacturer` varchar(20) NULL, `model_name` varchar(20) NULL, `model_id` varchar(20) NULL, `design_name` varchar(20) NULL, `product_name` varchar(20) NULL, `device_year_class` varchar(20) NULL, `supported_cpu_architectures` varchar(50) NULL, `os_name` varchar(20) NOT NULL, `os_version` varchar(20) NOT NULL, `os_build_id` varchar(20) NULL, `os_internal_build_id` varchar(20) NULL, `os_build_fingerprint` varchar(300) NULL, `platform_api_level` int NULL, `device_name` varchar(100) NULL, `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), `user_id` int NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB", undefined);
+        await queryRunner.query("CREATE TABLE `device` (`id` int NOT NULL AUTO_INCREMENT, `fcm_token` varchar(255) NULL, `uuid` varchar(36) NOT NULL, `is_device` tinyint NOT NULL, `brand` varchar(20) NULL, `manufacturer` varchar(20) NULL, `model_name` varchar(20) NULL, `model_id` varchar(20) NULL, `design_name` varchar(20) NULL, `product_name` varchar(20) NULL, `device_year_class` varchar(20) NULL, `supported_cpu_architectures` varchar(50) NULL, `os_name` varchar(20) NOT NULL, `os_version` varchar(20) NOT NULL, `os_build_id` varchar(20) NULL, `os_internal_build_id` varchar(20) NULL, `os_build_fingerprint` varchar(300) NULL, `platform_api_level` int NULL, `device_name` varchar(100) NULL, `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), `user_id` int NULL, INDEX `IDX_0683d0c6ce6c0327208a026da5` (`uuid`), UNIQUE INDEX `uuid` (`uuid`), PRIMARY KEY (`id`)) ENGINE=InnoDB", undefined);
         await queryRunner.query("CREATE TABLE `image` (`id` int NOT NULL AUTO_INCREMENT, `original_name` text NOT NULL, `name` text NOT NULL, `ext` varchar(10) NOT NULL, `size` int NOT NULL, `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), `post_id` int NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB", undefined);
         await queryRunner.query("CREATE TABLE `post_mapping` (`id` int NOT NULL AUTO_INCREMENT, `parent_id` int NULL, `child_id` int NULL, UNIQUE INDEX `parent_child` (`parent_id`, `child_id`), PRIMARY KEY (`id`)) ENGINE=InnoDB", undefined);
         await queryRunner.query("CREATE TABLE `unlike` (`id` int NOT NULL AUTO_INCREMENT, `status` tinyint NOT NULL DEFAULT 1, `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), `user_id` int NULL, `post_id` int NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB", undefined);
         await queryRunner.query("CREATE TABLE `post` (`id` int NOT NULL AUTO_INCREMENT, `title` varchar(30) NULL, `text` text NOT NULL, `status` enum ('public', 'private', 'delete') NOT NULL DEFAULT 'public', `order_id` int NOT NULL, `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), `user_id` int NULL, `parent_id` int NULL, UNIQUE INDEX `IDX_47251cc7a6cd6dfb78adad1871` (`order_id`), PRIMARY KEY (`id`)) ENGINE=InnoDB", undefined);
         await queryRunner.query("CREATE TABLE `like` (`id` int NOT NULL AUTO_INCREMENT, `status` tinyint NOT NULL DEFAULT 1, `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), `user_id` int NULL, `post_id` int NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB", undefined);
-        await queryRunner.query("CREATE TABLE `user` (`id` int NOT NULL AUTO_INCREMENT, `nickname` varchar(30) NOT NULL COMMENT '닉네임', `email` varchar(200) NOT NULL, `verified` tinyint NOT NULL DEFAULT 0, `access_token` varchar(100) NULL COMMENT '로그인시 업데이트되는 토큰', `gender` enum ('m', 'w') NOT NULL DEFAULT 'm', `birth_year` varchar(4) NOT NULL, `birth_month` varchar(2) NOT NULL, `birth_day` varchar(2) NOT NULL, `password` varchar(300) NOT NULL, `profile_photo` text NULL, `lat` float(12) NOT NULL DEFAULT 0, `lon` float(12) NOT NULL DEFAULT 0, `point` int NOT NULL DEFAULT 0, `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), PRIMARY KEY (`id`)) ENGINE=InnoDB", undefined);
-        await queryRunner.query("CREATE TABLE `message` (`id` int NOT NULL AUTO_INCREMENT, `text` text NOT NULL, `is_image` tinyint NOT NULL DEFAULT 0, `is_read` tinyint NOT NULL DEFAULT 0, `send_success` tinyint NOT NULL DEFAULT 0, `order_id` int NOT NULL, `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), `chat_id` int NULL, `user_id` int NULL, UNIQUE INDEX `IDX_a249d6abd5b2dacca134cfea12` (`order_id`), PRIMARY KEY (`id`)) ENGINE=InnoDB", undefined);
-        await queryRunner.query("CREATE TABLE `chat` (`id` int NOT NULL AUTO_INCREMENT, `name` varchar(255) NULL, `description` varchar(255) NULL, `max_persons` int NOT NULL DEFAULT 2, `password` varchar(255) NULL, `type` enum ('public', 'private', 'random', 'close') NOT NULL DEFAULT 'random', `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), PRIMARY KEY (`id`)) ENGINE=InnoDB", undefined);
+        await queryRunner.query("CREATE TABLE `user` (`id` int NOT NULL AUTO_INCREMENT, `nickname` varchar(30) NOT NULL COMMENT '닉네임', `email` varchar(200) NOT NULL, `sign_up_type` enum ('email', 'google', 'apple') NOT NULL DEFAULT 'email', `access_token` varchar(100) NULL COMMENT '로그인시 업데이트되는 토큰', `gender` enum ('m', 'w') NOT NULL DEFAULT 'm', `birth_year` varchar(4) NULL, `birth_month` varchar(2) NULL, `birth_day` varchar(2) NULL, `password` varchar(300) NULL, `profile_photo` text NULL, `lat` float(12) NOT NULL DEFAULT 0, `lon` float(12) NOT NULL DEFAULT 0, `point` int NOT NULL DEFAULT 0, `oauth_id` varchar(100) NULL, `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), PRIMARY KEY (`id`)) ENGINE=InnoDB", undefined);
+        await queryRunner.query("CREATE TABLE `message` (`id` int NOT NULL AUTO_INCREMENT, `text` text NOT NULL, `is_image` tinyint NOT NULL DEFAULT 0, `is_read` tinyint NOT NULL DEFAULT 0, `order_id` int NOT NULL, `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), `chat_id` int NULL, `user_id` int NULL, UNIQUE INDEX `IDX_a249d6abd5b2dacca134cfea12` (`order_id`), PRIMARY KEY (`id`)) ENGINE=InnoDB", undefined);
+        await queryRunner.query("CREATE TABLE `chat` (`id` int NOT NULL AUTO_INCREMENT, `name` varchar(255) NULL, `description` varchar(255) NULL, `max_persons` int NOT NULL DEFAULT 2, `password` varchar(255) NULL, `type` enum ('public', 'private', 'friend', 'random', 'close') NOT NULL DEFAULT 'random', `expiration_date` datetime NULL, `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), PRIMARY KEY (`id`)) ENGINE=InnoDB", undefined);
+        await queryRunner.query("CREATE TABLE `chat_queue` (`id` int NOT NULL AUTO_INCREMENT, `type` enum ('match', 'ready', 'out') NOT NULL DEFAULT 'ready', `user_id` int NOT NULL, `socket_id` varchar(255) NOT NULL, `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), INDEX `IDX_7c2083c235503359ed874e06f0` (`user_id`), PRIMARY KEY (`id`)) ENGINE=InnoDB", undefined);
         await queryRunner.query("CREATE TABLE `message_order_sequence` (`id` int NOT NULL AUTO_INCREMENT, PRIMARY KEY (`id`)) ENGINE=InnoDB", undefined);
         await queryRunner.query("CREATE TABLE `notification` (`id` int NOT NULL AUTO_INCREMENT, `text` varchar(255) NOT NULL, `type` enum ('like', 'unlike', 'reply', 'invite', 'accept', 'leave', 'expire', 'extend') NOT NULL, `target_id` int NOT NULL, `is_read` tinyint NOT NULL DEFAULT 0, `order_id` int NOT NULL, `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), `send_user_id` int NULL, `receive_user_id` int NULL, UNIQUE INDEX `IDX_3ea5cd8a1de9cbf90c86dd0582` (`order_id`), PRIMARY KEY (`id`)) ENGINE=InnoDB", undefined);
         await queryRunner.query("CREATE TABLE `notification_order_sequence` (`id` int NOT NULL AUTO_INCREMENT, PRIMARY KEY (`id`)) ENGINE=InnoDB", undefined);
         await queryRunner.query("CREATE TABLE `post_order_sequence` (`id` int NOT NULL AUTO_INCREMENT, PRIMARY KEY (`id`)) ENGINE=InnoDB", undefined);
         await queryRunner.query("CREATE TABLE `verification` (`id` int NOT NULL AUTO_INCREMENT, `key` text NOT NULL, `verified` tinyint NOT NULL DEFAULT 0, `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), `user_id` int NULL, UNIQUE INDEX `REL_49cf5e171603b309b419485046` (`user_id`), PRIMARY KEY (`id`)) ENGINE=InnoDB", undefined);
-        await queryRunner.query("CREATE TABLE `user_followers_user` (`user_id_1` int NOT NULL, `user_id_2` int NOT NULL, INDEX `IDX_bf1d134414d575c322caa8c15a` (`user_id_1`), INDEX `IDX_828cbfe05691311a43407ed84b` (`user_id_2`), PRIMARY KEY (`user_id_1`, `user_id_2`)) ENGINE=InnoDB", undefined);
         await queryRunner.query("CREATE TABLE `chat_users_user` (`chat_id` int NOT NULL, `user_id` int NOT NULL, INDEX `IDX_47bbc2c574f985aa11fa495336` (`chat_id`), INDEX `IDX_6f1fe66a3a27484ce383b25c15` (`user_id`), PRIMARY KEY (`chat_id`, `user_id`)) ENGINE=InnoDB", undefined);
         await queryRunner.query("ALTER TABLE `device` ADD CONSTRAINT `FK_ae7154510495c7ddda951b07a07` FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION", undefined);
         await queryRunner.query("ALTER TABLE `image` ADD CONSTRAINT `FK_595c60d3e7e8edf1cc0912782bd` FOREIGN KEY (`post_id`) REFERENCES `post`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION", undefined);
@@ -35,8 +35,6 @@ export class init1588145148340 implements MigrationInterface {
         await queryRunner.query("ALTER TABLE `notification` ADD CONSTRAINT `FK_3943096480eaf789bf76674657b` FOREIGN KEY (`send_user_id`) REFERENCES `user`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION", undefined);
         await queryRunner.query("ALTER TABLE `notification` ADD CONSTRAINT `FK_c96fc5f872aa474f2e454cddc29` FOREIGN KEY (`receive_user_id`) REFERENCES `user`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION", undefined);
         await queryRunner.query("ALTER TABLE `verification` ADD CONSTRAINT `FK_49cf5e171603b309b4194850461` FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION", undefined);
-        await queryRunner.query("ALTER TABLE `user_followers_user` ADD CONSTRAINT `FK_bf1d134414d575c322caa8c15ad` FOREIGN KEY (`user_id_1`) REFERENCES `user`(`id`) ON DELETE CASCADE ON UPDATE NO ACTION", undefined);
-        await queryRunner.query("ALTER TABLE `user_followers_user` ADD CONSTRAINT `FK_828cbfe05691311a43407ed84b0` FOREIGN KEY (`user_id_2`) REFERENCES `user`(`id`) ON DELETE CASCADE ON UPDATE NO ACTION", undefined);
         await queryRunner.query("ALTER TABLE `chat_users_user` ADD CONSTRAINT `FK_47bbc2c574f985aa11fa495336f` FOREIGN KEY (`chat_id`) REFERENCES `chat`(`id`) ON DELETE CASCADE ON UPDATE NO ACTION", undefined);
         await queryRunner.query("ALTER TABLE `chat_users_user` ADD CONSTRAINT `FK_6f1fe66a3a27484ce383b25c15c` FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON DELETE CASCADE ON UPDATE NO ACTION", undefined);
     }
@@ -44,8 +42,6 @@ export class init1588145148340 implements MigrationInterface {
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query("ALTER TABLE `chat_users_user` DROP FOREIGN KEY `FK_6f1fe66a3a27484ce383b25c15c`", undefined);
         await queryRunner.query("ALTER TABLE `chat_users_user` DROP FOREIGN KEY `FK_47bbc2c574f985aa11fa495336f`", undefined);
-        await queryRunner.query("ALTER TABLE `user_followers_user` DROP FOREIGN KEY `FK_828cbfe05691311a43407ed84b0`", undefined);
-        await queryRunner.query("ALTER TABLE `user_followers_user` DROP FOREIGN KEY `FK_bf1d134414d575c322caa8c15ad`", undefined);
         await queryRunner.query("ALTER TABLE `verification` DROP FOREIGN KEY `FK_49cf5e171603b309b4194850461`", undefined);
         await queryRunner.query("ALTER TABLE `notification` DROP FOREIGN KEY `FK_c96fc5f872aa474f2e454cddc29`", undefined);
         await queryRunner.query("ALTER TABLE `notification` DROP FOREIGN KEY `FK_3943096480eaf789bf76674657b`", undefined);
@@ -64,9 +60,6 @@ export class init1588145148340 implements MigrationInterface {
         await queryRunner.query("DROP INDEX `IDX_6f1fe66a3a27484ce383b25c15` ON `chat_users_user`", undefined);
         await queryRunner.query("DROP INDEX `IDX_47bbc2c574f985aa11fa495336` ON `chat_users_user`", undefined);
         await queryRunner.query("DROP TABLE `chat_users_user`", undefined);
-        await queryRunner.query("DROP INDEX `IDX_828cbfe05691311a43407ed84b` ON `user_followers_user`", undefined);
-        await queryRunner.query("DROP INDEX `IDX_bf1d134414d575c322caa8c15a` ON `user_followers_user`", undefined);
-        await queryRunner.query("DROP TABLE `user_followers_user`", undefined);
         await queryRunner.query("DROP INDEX `REL_49cf5e171603b309b419485046` ON `verification`", undefined);
         await queryRunner.query("DROP TABLE `verification`", undefined);
         await queryRunner.query("DROP TABLE `post_order_sequence`", undefined);
@@ -74,6 +67,8 @@ export class init1588145148340 implements MigrationInterface {
         await queryRunner.query("DROP INDEX `IDX_3ea5cd8a1de9cbf90c86dd0582` ON `notification`", undefined);
         await queryRunner.query("DROP TABLE `notification`", undefined);
         await queryRunner.query("DROP TABLE `message_order_sequence`", undefined);
+        await queryRunner.query("DROP INDEX `IDX_7c2083c235503359ed874e06f0` ON `chat_queue`", undefined);
+        await queryRunner.query("DROP TABLE `chat_queue`", undefined);
         await queryRunner.query("DROP TABLE `chat`", undefined);
         await queryRunner.query("DROP INDEX `IDX_a249d6abd5b2dacca134cfea12` ON `message`", undefined);
         await queryRunner.query("DROP TABLE `message`", undefined);
@@ -85,6 +80,8 @@ export class init1588145148340 implements MigrationInterface {
         await queryRunner.query("DROP INDEX `parent_child` ON `post_mapping`", undefined);
         await queryRunner.query("DROP TABLE `post_mapping`", undefined);
         await queryRunner.query("DROP TABLE `image`", undefined);
+        await queryRunner.query("DROP INDEX `uuid` ON `device`", undefined);
+        await queryRunner.query("DROP INDEX `IDX_0683d0c6ce6c0327208a026da5` ON `device`", undefined);
         await queryRunner.query("DROP TABLE `device`", undefined);
     }
 
